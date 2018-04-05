9449c65dcd26ee489e9b4748996314a6e2a4a99d
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-05 16:07:28 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,32 +1,32 @@
 INSERT INTO bears
   (name, age, gender, color, temperament, alive)
   VALUES
-  ('Mr. Chocolate', 3, 'M', 'Brown', 'Mellow', 'true');
+  ('Mr. Chocolate', 3, 'M', 'Brown', 'Mellow', 1);
 INSERT INTO bears
   (name, age, gender, color, temperament, alive)
     VALUES
-  ('Rowdy', 5, 'M', 'Black', 'Loud', 'true');
+  ('Rowdy', 5, 'M', 'Black', 'Loud', 1);
 INSERT INTO bears
   (name, age, gender, color, temperament, alive)
     VALUES
-  ('Tabitha', 2, 'F', 'Cream', 'Sleepy', 'true');
+  ('Tabitha', 2, 'F', 'Cream', 'Sleepy', 1);
 INSERT INTO bears
   (name, age, gender, color, temperament, alive)
     VALUES
-  ('Sergeant Brown', 8, 'M', 'Brown', 'Bossy', 'true');
+  ('Sergeant Brown', 8, 'M', 'Brown', 'Bossy', 1);
 INSERT INTO bears
   (name, age, gender, color, temperament, alive)
     VALUES
-  ('Melissa', 5, 'F', 'Black', 'Mellow', 'true');
+  ('Melissa', 5, 'F', 'Black', 'Mellow', 1);
 INSERT INTO bears
   (name, age, gender, color, temperament, alive)
     VALUES
-  ('Grinch', 7, 'M', 'Brown', 'Angry', 'true');
+  ('Grinch', 7, 'M', 'Brown', 'Angry', 1);
 INSERT INTO bears
   (name, age, gender, color, temperament, alive)
     VALUES
-  ('Wendy', 4, 'F', 'Ginger', 'Friendly', 'true');
+  ('Wendy', 4, 'F', 'Ginger', 'Friendly', 1);
 INSERT INTO bears
   (age, gender, color, temperament, alive)
     VALUES
-  (6, 'M', 'Brown', 'Mean', 'true');
+  (6, 'M', 'Brown', 'Mean', 1);
