30b1025855132015bb6c8b99e3f51765bcbef8ff
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-05 15:56:04 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,11 @@ INSERT INTO bears
   (name, age, gender, color, temperament, alive)
   VALUES
   ('Mr. Chocolate', 3, 'M', 'Brown', 'Mellow', 'true');
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+    VALUES
+  ('Rowdy', 5, 'M', 'Black', 'Loud', 'true');
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+    VALUES
+  ('Tabitha', 2, 'F', 'Cream', 'Sleepy', 'true');
